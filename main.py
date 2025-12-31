4a319212be7ef99b9d9863c65620af381637ff83
1837669410 <1837669410@qq.com>
2025-12-31 19:45:44 +0800

    add main.py code

--- main.py
+++ main.py
@@ -8,12 +8,12 @@ from topic import compute_topic_heat
 
 if __name__ == '__main__':
     # 1. 获取 trending
-    get_trending(languages = ['java'])
+    get_trending(languages = ['python', 'c++', 'c', 'java', 'c#', 'javascript', 'go', 'rust'])
     # 2. 分析 trending topic
     with open('trending.json', 'r', encoding = 'utf-8') as f:
         data = json.load(f)
     tagged = [tag_repo(repo) for repo in data]
     buckets = aggregate_by_best_topic(tagged)
     topic_heat = compute_topic_heat(buckets)
-    # 3. 打印结果
-    print_topics_cli_rich(topic_heat)
+    # # 3. 打印结果
+    print_topics_cli_rich(topic_heat, top_k_topics = 5, top_k_repos = 5)
