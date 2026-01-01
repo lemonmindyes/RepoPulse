01fd8dfb63aafcee179abf31497eaf810cf847d3
1837669410 <1837669410@qq.com>
2026-01-01 12:46:39 +0800

    add languages selection function and add cli args function

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import json
+import argparse
 
 from analysis import tag_repo, aggregate_by_best_topic
 from cli import print_topics_cli_rich
@@ -7,9 +8,34 @@ from topic import compute_topic_heat
 
 
 if __name__ == '__main__':
-    date_range = 'daily'
+    parser = argparse.ArgumentParser(
+        description = 'Analyze trending topics from GitHub repositories\n'
+                      '分析 GitHub Trending 仓库的热门话题'
+    )
+    # time range（分析时间范围）
+    parser.add_argument(
+        '--time-range',
+        choices = ['daily', 'weekly', 'monthly'],
+        default = 'daily',
+        help = 'Time range for analysis (daily/weekly/monthly) (default: daily)\n'
+               '分析时间范围 (daily/weekly/monthly)'
+    )
+
+    # languages (语言列表)
+    parser.add_argument(
+        '--languages',
+        nargs = '+',
+        default = ['python', 'c++', 'c', 'java', 'javascript', 'typescript', 'go', 'rust'],
+        help = 'List of programming languages to analyze (default: python c++ c java javascript typescript go rust)\n'
+               '任何要分析的编程语言列表'
+    )
+
+    args = parser.parse_args()
+
     # 1. 获取 trending
-    get_trending(languages = ['python', 'c++', 'c', 'java', 'javascript', 'typescript', 'go', 'rust'], date_range = date_range)
+    print(args.languages)
+    get_trending(languages = args.languages,
+                 time_range = args.time_range)
     # 2. 分析 trending topic
     with open('trending.json', 'r', encoding = 'utf-8') as f:
         data = json.load(f)
@@ -17,4 +43,4 @@ if __name__ == '__main__':
     buckets = aggregate_by_best_topic(tagged)
     topic_heat = compute_topic_heat(buckets)
     # # 3. 打印结果
-    print_topics_cli_rich(topic_heat, top_k_topics = 5, top_k_repos = 5, date_range = date_range)
+    print_topics_cli_rich(topic_heat, top_k_topics = 5, top_k_repos = 5, time_range = args.time_range)
