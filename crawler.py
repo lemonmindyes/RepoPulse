63d7cfcd522b6b2467c2df2cf9cae71e1e4adb4d
1837669410 <1837669410@qq.com>
2026-01-07 20:20:32 +0800

    add new async crawler

--- crawler.py
+++ crawler.py
@@ -1,6 +1,10 @@
+import asyncio
+import certifi
 import json
+import socket
+import ssl
 
-import requests
+import aiohttp
 from fake_useragent import UserAgent
 from lxml import etree
 
@@ -33,7 +37,54 @@ def keep_latest_repo(data: list[dict]) -> list[dict]:
 #     return repo_topics
 
 
-def get_trending(languages: list[str] = None, time_range: str = 'daily'):
+async def parse_trending_page(session: aiohttp.ClientSession, url: str, semaphore: asyncio.Semaphore):
+    async with semaphore:
+        async with session.get(url, proxy = 'http://127.0.0.1:7890') as response:
+            html = await response.text()
+
+    tree = etree.HTML(html)
+    articles = tree.xpath('//div[@class="Box"]/div[2]/article')
+
+    page_data = []
+
+    for article in articles:
+        repo_author = article.xpath('./h2/a/span/text()')[0].replace(' /', '').strip()  # 仓库作者
+        repo_name = ''.join(article.xpath('./h2/a/text()')).strip()  # 仓库名称
+        # 仓库描述
+        if not article.xpath('./p/text()'):
+            repo_describe = ''
+        else:
+            repo_describe = article.xpath('./p/text()')[0].strip()
+        # 仓库语言
+        if not article.xpath('./div[2]/span[@class="d-inline-block ml-0 mr-3"]/span[2]/text()'):
+            repo_language = ''
+        else:
+            repo_language = article.xpath('./div[2]/span[@class="d-inline-block ml-0 mr-3"]/span[2]/text()')[0]
+        repo_stars = article.xpath('./div[2]/a[1]/text()')[0].strip().replace(',', '')  # 仓库 stars
+        repo_forks = article.xpath('./div[2]/a[2]/text()')[0].strip().replace(',', '')  # 仓库 forks
+        # 今日 stars
+        texts = article.xpath('./div[2]/span[@class="d-inline-block float-sm-right"]/text()')
+        raw = ''.join(texts).strip()
+        added_stars = raw.replace(',', '').split()[0]
+        # 获取仓库详细页面信息
+        # repo_url = f'{base_repo_url}/{repo_author}/{repo_name}'
+        # repo_topics = get_repo_detail_info(repo_url, headers)
+
+        # 保存信息
+        page_data.append({
+            'repo_author': repo_author,
+            'repo_name': repo_name,
+            'repo_describe': repo_describe,
+            'repo_language': repo_language,
+            'repo_stars': repo_stars,
+            'repo_forks': repo_forks,
+            'added_stars': added_stars,
+            # 'repo_topics': repo_topics
+        })
+    return page_data
+
+
+async def get_trending_async(languages: list[str] | None = None, time_range: str = 'daily'):
     if languages is None:
         # 预置一些常见语言的趋势页面
         article_urls = [
@@ -41,63 +92,44 @@ def get_trending(languages: list[str] = None, time_range: str = 'daily'):
             f'https://github.com/trending/python?since={time_range}', # python trending页面
             f'https://github.com/trending/go?since={time_range}', # go trending页面
             f'https://github.com/trending/c?since={time_range}', # c trending页面
-            f'https://github.com/trending/c++?since={time_range}' # c++ trending页面
+            f'https://github.com/trending/c++?since={time_range}', # c++ trending页面
             f'https://github.com/trending/javascript?since={time_range}', # javascript trending页面
-            f'https://github.com/trending/typescript?since={time_range}', # typescript trending页面
+            f'https://github.com/trending/typescript?since={time_range}' # typescript trending页面
         ]
     else:
         article_urls = ['https://github.com/trending'] # 默认 trending页面
         for language in languages:
             article_urls.append(f'https://github.com/trending/{language}?since={time_range}')
+
     headers = {
-        'User-Agent': UserAgent().random
+        'User-Agent': UserAgent().edge
     }
-    data = []
+    ssl_context = ssl.create_default_context(
+        cafile = certifi.where()
+    )
+    connector = aiohttp.TCPConnector(ssl = ssl_context, family = socket.AF_INET, happy_eyeballs_delay = None)
 
-    base_repo_url = 'https://github.com'
-    for article_url in article_urls:
-        response = requests.get(article_url, headers = headers, timeout = 5)
-        tree = etree.HTML(response.text)
+    semaphore = asyncio.Semaphore(10) # 限制并发数量
 
-        articles = tree.xpath('//div[@class="Box"]/div[2]/article')
-        for article in articles:
-            repo_author = article.xpath('./h2/a/span/text()')[0].replace(' /', '').strip() # 仓库作者
-            repo_name = ''.join(article.xpath('./h2/a/text()')).strip() # 仓库名称
-            # 仓库描述
-            if not article.xpath('./p/text()'):
-                repo_describe = ''
-            else:
-                repo_describe = article.xpath('./p/text()')[0].strip()
-            # 仓库语言
-            if not article.xpath('./div[2]/span[@class="d-inline-block ml-0 mr-3"]/span[2]/text()'):
-                repo_language = ''
-            else:
-                repo_language = article.xpath('./div[2]/span[@class="d-inline-block ml-0 mr-3"]/span[2]/text()')[0]
-            repo_stars = article.xpath('./div[2]/a[1]/text()')[0].strip().replace(',', '') # 仓库 stars
-            repo_forks = article.xpath('./div[2]/a[2]/text()')[0].strip().replace(',', '') # 仓库 forks
-            # 今日 stars
-            texts = article.xpath('./div[2]/span[@class="d-inline-block float-sm-right"]/text()')
-            raw = ''.join(texts).strip()
-            added_stars = raw.replace(',', '').split()[0]
-            # 获取仓库详细页面信息
-            # repo_url = f'{base_repo_url}/{repo_author}/{repo_name}'
-            # repo_topics = get_repo_detail_info(repo_url, headers)
+    async with aiohttp.ClientSession(headers = headers, connector = connector) as session:
+        tasks = [parse_trending_page(session, url, semaphore) for url in article_urls]
+        results = await asyncio.gather(*tasks)
 
-            # 保存信息
-            data.append({
-                'repo_author': repo_author,
-                'repo_name': repo_name,
-                'repo_describe': repo_describe,
-                'repo_language': repo_language,
-                'repo_stars': repo_stars,
-                'repo_forks': repo_forks,
-                'added_stars': added_stars,
-                # 'repo_topics': repo_topics
-            })
+    data = [item for page in results for item in page]
     data = keep_latest_repo(data)
+
     with open('trending.json', 'w', encoding = 'utf-8') as f:
         json.dump(data, f, ensure_ascii = False, indent = 4)
 
 
+def get_trending(languages: list[str] | None = None, time_range: str = 'daily'):
+    asyncio.run(get_trending_async(languages, time_range))
+
+
 if __name__ == '__main__':
+    import time
+    start = time.time()
     get_trending()
+    print(f'耗时：{time.time() - start}秒')
+
+
