fa873159c5b09a7aac8267b6e27793d039632de8
1837669410 <1837669410@qq.com>
2026-01-14 17:26:00 +0800

    add top_k_topics and top_k_repos argument

--- main.py
+++ main.py
@@ -25,9 +25,26 @@ if __name__ == '__main__':
     parser.add_argument(
         '--languages',
         nargs = '+',
-        default = ['python', 'c++', 'c', 'java', 'javascript', 'typescript', 'go', 'rust'],
-        help = 'List of programming languages to analyze (default: python c++ c java javascript typescript go rust)\n'
+        default = ['python', 'c++', 'c', 'java', 'javascript', 'typescript', 'go', 'rust', 'shell'],
+        help = 'List of programming languages to analyze '
+               '(default: python c++ c java javascript typescript go rust shell)\n'
                '任何要分析的编程语言列表'
+    )
+
+    # top_k_topics (输出主题数)
+    parser.add_argument(
+        '--top-k-topics',
+        type = int,
+        default = 5,
+        help = 'Number of topics to print (default: 5)\n'
+    )
+
+    # top_k_repos (输出仓库数)
+    parser.add_argument(
+        '--top-k-repos',
+        type = int,
+        default = 5,
+        help = 'Number of repositories to print (default: 5)\n'
     )
 
     args = parser.parse_args()
@@ -42,4 +59,9 @@ if __name__ == '__main__':
     buckets = aggregate_by_best_topic(tagged)
     topic_heat = compute_topic_heat(buckets)
     # # 3. 打印结果
-    print_topics_cli_rich(topic_heat, top_k_topics = 5, top_k_repos = 5, time_range = args.time_range)
+    print_topics_cli_rich(
+        topic_heat,
+        top_k_topics = args.top_k_topics,
+        top_k_repos = args.top_k_repos,
+        time_range = args.time_range
+    )
